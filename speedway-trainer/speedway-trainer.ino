951204658b302d0c7f6b2d79b9b61bd843f3aa88
danielgorzolnik <danielgorzolnik@gmail.com>
2020-03-12 16:39:18 +0100

    obsluga enkodera

--- speedway-trainer/speedway-trainer.ino
+++ speedway-trainer/speedway-trainer.ino
@@ -5,11 +5,39 @@
 
 LiquidCrystal lcd(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7);
 
+//variables
+uint8_t encoder = 0, encoder_t = 0;
+
 void setup(){
   configPins();
   lcd.begin(lcd_columns, lcd_rows); //init lcd display
   lcd.print("speedway trainer");
+  encoder = encoder_read();
+
+  Serial.begin(9600);
 }
 
 void loop() {
+  //encoder
+  encoder_t = encoder_read();
+  if (encoder != encoder_t) {
+    if ((encoder==3 && encoder_t==1)) encoder_left();
+    else if ((encoder==2 && encoder_t==0)) encoder_right();
+    encoder = encoder_t;
+  }
+}
+
+void encoder_left(){ //encoder move - left
+  Serial.println("left");
+}
+
+void encoder_right(){ //encoder move - right
+  Serial.println("right");
+}
+
+uint8_t encoder_read() { //read encoder state
+  uint8_t val = 0;
+  if(!digitalRead(encoder_in1)) val |= (1 << 1);
+  if(!digitalRead(encoder_in2)) val |= (1 << 0);
+  return val;
 }
